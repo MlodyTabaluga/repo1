2a5c6053607dc1b82497c00ca54b9588ea108618
Miłosz Szubryt <mszubryt2005@gmail.com>
2024-03-04 20:17:34 +0100

    chujeczka cipaczka

---
+++ repo1/repo1/DanePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace repo1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DanePage : ContentPage
+    {
+        public DanePage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listaBMI.ItemsSource = await App.Database.WyswietlUzytkownikaAsync();
+        }
+
+        private void Wroc_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}

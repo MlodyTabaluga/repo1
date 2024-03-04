38ddcf3aeb74e9d435a53768f951606103d1c2b8
KacperMol <kacpermol450@gmail.com>
2024-03-04 20:00:48 +0100

    paczka nr.2 wypierdolona

--- repo1/repo1/MainPage.xaml.cs
+++ repo1/repo1/MainPage.xaml.cs
@@ -13,6 +13,29 @@ namespace repo1
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+        private async void Zapisz_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(imietxt.Text) && !string.IsNullOrWhiteSpace(nazwiskotxt.Text))
+            {
+                await App.Database.ZapiszUzytkownikaAsync(new Uzytkownik
+                {
+                    Imie = imietxt.Text,
+                    Nazwisko = nazwiskotxt.Text,
+                    BMI = double.Parse(BMI.Text),
+                });
+
+                imietxt.Text = nazwiskotxt.Text = string.Empty;
+                await DisplayAlert("Sukces", "Dodano dane do bazy!", "OK");
+            }
+            else
+                await DisplayAlert("Błąd", "Aby zapisać musisz wpisać imie i nazwisko!", "OK");
+        }
+
+        private void ZobaczListe_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DanePage());
         }
     }
 }

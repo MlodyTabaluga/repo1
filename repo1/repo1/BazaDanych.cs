1dd70d32015de25e944eb24cf8813549415fbfbf
KacperMol <kacpermol450@gmail.com>
2024-03-04 20:06:24 +0100

    jebana baza w pizde

---
+++ repo1/repo1/BazaDanych.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repo1
+{
+    public class BazaDanych
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public BazaDanych(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Uzytkownik>().Wait();
+        }
+        public Task<int> ZapiszUzytkownikaAsync(Uzytkownik uzytkownik)
+        {
+            return _database.InsertAsync(uzytkownik);
+        }
+        public Task<List<Uzytkownik>> WyswietlUzytkownikaAsync()
+        {
+            return _database.Table<Uzytkownik>().ToListAsync();
+        }
+    }
+}
